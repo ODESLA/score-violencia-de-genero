6017a296c228bea970b92f7e636cffebf0030f3e
Leonardo <leonardo@42mate.com>
2020-11-13 10:46:25 -0300

    implemented fuzzy search

--- src/d2_violencia_de_genero/procesar_pdf.py
+++ src/d2_violencia_de_genero/procesar_pdf.py
@@ -5,6 +5,7 @@ import logging
 from pathlib import Path
 from glob import glob
 
+from fuzzysearch import find_near_matches
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfinterp import PDFPageInterpreter
@@ -13,46 +14,99 @@ from pdfminer.converter import TextConverter
 logger = logging.getLogger("main")
 
 
-
-def extract_text_from_pdf(pdf_path):
+def filter_pages(all_pages, dist):
     """
-    Helper function to extract the plain text from .pdf files
+    Function to filter elements of a list based on the max distance allowed between consecutive elements
+    Args:
+        all_pages: list of integers
+        dist: max distance allow between consecutive elements of the all_pages list
 
-    :param pdf_path: path to PDF file to be extracted
-    :return: iterator of string of extracted text
+    Returns: list af integers that meet the max distance criteria
     """
-    with open(pdf_path, 'rb') as fh:
-        for page in PDFPage.get_pages(fh,
-                                      caching=True,
-                                      check_extractable=True):
-            resource_manager = PDFResourceManager()
-            fake_file_handle = io.StringIO()
-            converter = TextConverter(resource_manager, fake_file_handle)
-            page_interpreter = PDFPageInterpreter(resource_manager, converter)
-            page_interpreter.process_page(page)
+    new_pages = []
+    for i in range(len(all_pages) - 1):
+        if (all_pages[i + 1] - all_pages[i]) < dist:
+            new_pages.append(all_pages[i])
+        elif (i > 0) and (all_pages[i] - all_pages[i - 1] < dist):
+            new_pages.append(all_pages[i])
+    return new_pages
 
-            text = fake_file_handle.getvalue()
-            yield text
 
-            # close open handles
-            converter.close()
-            fake_file_handle.close()
+def extract_text_from_pdf(pdf_path, full=False, pages=()):
+    """
+    Helper function to extract the plain text from PDF files
+    Args:
+        pdf_path: path to PDF file to be extracted
+        full: optional boolean parameter to indicate if all the pages must be processed
+        pages: range of pages that needs to be extracted
+
+    Returns: iterator of string of extracted text
+    """
+
+    if full:
+        with open(pdf_path, "rb") as pdf:
+            for page in PDFPage.get_pages(pdf, caching=True, check_extractable=True):
+                resource_manager = PDFResourceManager()
+                fake_file_handle = io.StringIO()
+                converter = TextConverter(resource_manager, fake_file_handle)
+                page_interpreter = PDFPageInterpreter(resource_manager, converter)
+                page_interpreter.process_page(page)
+
+                text = fake_file_handle.getvalue()
+                yield text
+
+                # close open handles
+                converter.close()
+                fake_file_handle.close()
+    elif pages:
+        with open(pdf_path, "rb") as pdf:
+            for page_number, page in enumerate(
+                PDFPage.get_pages(pdf, caching=True, check_extractable=True)
+            ):
+                resource_manager = PDFResourceManager()
+                fake_file_handle = io.StringIO()
+                converter = TextConverter(resource_manager, fake_file_handle)
+                page_interpreter = PDFPageInterpreter(resource_manager, converter)
+                if pages[0] <= page_number <= pages[1]:
+                    page_interpreter.process_page(page)
+                    text = fake_file_handle.getvalue()
+                    yield text
+                else:
+                    yield ""
+
+                    # close open handles
+                converter.close()
+                fake_file_handle.close()
 
 
 def extract_information(file):
     path = Path(file)
-    # TODO Mejorar la lógica para detectar las páginas con el copete. Sugerencia: usar Regex
+    # TODO Utilizar OCR cuando no se puede extraer el texto del pdf
 
-    string1 = 'Corte Suprema de Justicia de la Nación'
-    string2 = 'Oficina de Violencia Doméstica'
+    string_1 = "corte suprema de justicia de la nación"
+    string_2 = "oficina de violencia doméstica"
 
-    with open(f'{path.stem}.txt', 'w', encoding='UTF8', errors='ignore') as f:
-        i = 1
-        for text in extract_text_from_pdf(path):
-            if string1 in text and string2 in text:
-                f.write(text)
-                f.write(f'============ page {i} ==============')
-                i += 1
+    i = 0
+    all_pages = []
+    for text in extract_text_from_pdf(path, full=True):
+        matches_1 = find_near_matches(string_1, text.lower(), max_l_dist=4)
+        matches_2 = find_near_matches(string_2, text.lower(), max_l_dist=4)
+        if matches_1 and matches_2:
+            all_pages.append(i)
+        i += 1
+
+    if len(all_pages) > 2:
+        all_pages = filter_pages(all_pages, 10)
+        all_pages = sorted(all_pages)
+        with open(f"{path.stem}.txt", "w", encoding="UTF8", errors="ignore") as f:
+            for text in extract_text_from_pdf(
+                path, pages=(all_pages[0], all_pages[-1] + 1)
+            ):
+                if len(text) > 0:
+                    f.write("============ page ==============")
+                    f.write(text)
+    else:
+        print("No se encontró ninguna página de la OVD")
 
 
 def main(file, all_files):
@@ -64,19 +118,18 @@ def main(file, all_files):
         path = str(Path(os.getcwd()) / "*.pdf")
         files = glob(path)
         for file in files:
-            print(f'procesando {file}')
+            print(f"procesando {file}")
             extract_information(file)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Extraer información de la OVD"
-    )
+    parser = argparse.ArgumentParser(description="Extraer información de la OVD")
+    # TODO permitir que se pueda especificar la carpeta a procesar
     parser.add_argument(
         "-a",
         "--all",
         action="store_true",
-        dest='all_files',
+        dest="all_files",
         default=False,
         required=False,
         help="Process all files in the root folder",
@@ -98,7 +151,7 @@ if __name__ == "__main__":
             logger.error("El archivo no existe")
             exit(1)
 
-        if file_path.exists() and Path(file_path).suffix[1:].lower() != 'pdf':
+        if file_path.exists() and Path(file_path).suffix[1:].lower() != "pdf":
             logger.error("El archivo especificado no es un pdf")
             exit(1)
 
@@ -107,11 +160,11 @@ if __name__ == "__main__":
             exit(1)
 
     if args.file and args.all_files:
-        logger.error('Los argumentos suministrados son excluyentes')
+        logger.error("Los argumentos suministrados son excluyentes")
         exit(1)
 
     if not args.file and not args.all_files:
-        logger.error('Se debe suministrar al menos un argumento')
+        logger.error("Se debe suministrar al menos un argumento")
         exit(1)
 
     main(args.file, args.all_files)
